bd73b3d891d596955afd27ee4e0308c0b9192e4c
Ayush Shrivastava <shrivastava.ayush1996@gmail.com>
2018-04-13 03:08:02 +0530

    Added train_split argument

--- data/prepro_utils.lua
+++ data/prepro_utils.lua
@@ -36,10 +36,17 @@ function extractFeatures(model, opt, ndims, preprocessFn)
         table.insert(valList, string.format('%s/val2014/COCO_val2014_%012d.jpg', opt.imageRoot, imName))
     end
 
+    if opt.train_split == 'trainval' then
+        local testList = {}
+        for i, imName in pairs(jsonFile.unique_img_test) do
+            table.insert(testList, string.format('%s/test2014/COCO_test2014_%012d.jpg', opt.imageRoot, imName))
+        end
+    end
+
     local sz = #trainList
     local trainFeats = torch.FloatTensor(sz, unpack(ndims))
     -- feature_dims shall be either 2 (NW format), else 4 (having NCHW format)
-    local feature_dims = #trainFeats:size()  
+    local feature_dims = #trainFeats:size()
     print(string.format('Processing %d train images...', sz))
     for i = 1, sz, opt.batchSize do
         xlua.progress(i, sz)
@@ -90,8 +97,41 @@ function extractFeatures(model, opt, ndims, preprocessFn)
         collectgarbage()
     end
 
+    print('\n')
+
+    if opt.train_split == 'trainval' then
+
+        local sz = #testList
+        local testFeats = torch.FloatTensor(sz, unpack(ndims))
+        print(string.format('Processing %d test images...', sz))
+        for i = 1, sz, opt.batchSize do
+            xlua.progress(i, sz)
+            r = math.min(sz, i + opt.batchSize - 1)
+            ims = torch.DoubleTensor(r - i + 1, 3, opt.imgSize, opt.imgSize)
+            for j = 1, r - i + 1 do
+                ims[j] = loadImage(valList[i + j - 1], opt.imgSize)
+                ims[j] = preprocessFn(ims[j])
+            end
+            if opt.gpuid >= 0 then
+                ims = ims:cuda()
+            end
+
+            if feature_dims == 4 then
+                -- forward pass and permute to get NHWC format
+                model:forward(ims):permute(1, 3, 4, 2):contiguous():float()
+            else
+                model:forward(ims)
+            end
+            testFeats[{{i, r}, {}}] = model.output:float()
+            collectgarbage()
+        end
+    end
+
     local h5File = hdf5.open(opt.outName, 'w')
     h5File:write('/images_train', trainFeats)
     h5File:write('/images_val', valFeats)
+    if opt.train_split == 'trainval' then
+        h5File:write('/images_test', testFeats)
+    end
     h5File:close()
 end
