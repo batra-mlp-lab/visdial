fe1bb03ed8d3e9a05c923e15550e16a3a2cc3555
Karan Desai <karandesai281196@gmail.com>
2018-04-21 00:33:42 +0530

    Modify prepro_utils.lua to preprocess test split images.

--- data/prepro_utils.lua
+++ data/prepro_utils.lua
@@ -36,6 +36,11 @@ function extractFeatures(model, opt, ndims, preprocessFn)
         table.insert(valList, string.format('%s/val2014/COCO_val2014_%012d.jpg', opt.imageRoot, imName))
     end
 
+    local testList = {}
+    for i, imName in pairs(jsonFile.unique_img_test) do
+        table.insert(valList, string.format('%s/test2015/COCO_test2015_%012d.jpg', opt.imageRoot, imName))
+    end
+
     local sz = #trainList
     local trainFeats = torch.FloatTensor(sz, unpack(ndims))
     -- feature_dims shall be either 2 (NW format), else 4 (having NCHW format)
@@ -90,8 +95,34 @@ function extractFeatures(model, opt, ndims, preprocessFn)
         collectgarbage()
     end
 
+    local sz = #testList
+    local testFeats = torch.FloatTensor(sz, unpack(ndims))
+    print(string.format('Processing %d test images...', sz))
+    for i = 1, sz, opt.batchSize do
+        xlua.progress(i, sz)
+        r = math.min(sz, i + opt.batchSize - 1)
+        ims = torch.DoubleTensor(r - i + 1, 3, opt.imgSize, opt.imgSize)
+        for j = 1, r - i + 1 do
+            ims[j] = loadImage(valList[i + j - 1], opt.imgSize)
+            ims[j] = preprocessFn(ims[j])
+        end
+        if opt.gpuid >= 0 then
+            ims = ims:cuda()
+        end
+
+        if feature_dims == 4 then
+            -- forward pass and permute to get NHWC format
+            model:forward(ims):permute(1, 3, 4, 2):contiguous():float()
+        else
+            model:forward(ims)
+        end
+        testFeats[{{i, r}, {}}] = model.output:float()
+        collectgarbage()
+    end
+
     local h5File = hdf5.open(opt.outName, 'w')
     h5File:write('/images_train', trainFeats)
     h5File:write('/images_val', valFeats)
+    h5File:write('/images_test', testFeats)
     h5File:close()
 end
