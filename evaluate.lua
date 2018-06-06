3ffb761dc6725e2f5e70a8c8371c1c457cf57bbb
Abhishek Das <das.abhshk@gmail.com>
2018-06-06 17:05:15 -0400

    Removes option defaults for older checkpoints

--- evaluate.lua
+++ evaluate.lua
@@ -60,11 +60,6 @@ local savedModel = torch.load(opt.loadPath)
 -- transfer all options to model
 local modelParams = savedModel.modelParams
 
--- default values of these opts to load models saved with older version of code
-if not modelParams.weightInit then modelParams.weightInit = 'xavier'; end
-if not modelParams.saveIter then modelParams.saveIter = 2; end
-if not modelParams.imgSpatialSize then modelParams.imgSpatialSize = 14; end
-
 opt.imgNorm = modelParams.imgNorm
 opt.encoder = modelParams.encoder
 opt.decoder = modelParams.decoder
